6a89aa529b13dd9dd938ab905d9d914e0de210bc
B <contact@byronlevene.net>
2016-02-06 23:54:22 +0800

    get back to this sometime...

---
+++ start.php
@@ -0,0 +1,32 @@
+<?php
+
+/*user variables */
+
+$irl_soul_me = "[myName]";
+$irl_soul_you = "[yourName]";
+$irl_gender_me = 
+$irl_gender_you =
+
+$disposition_me = array[moody, bitchy, happy, drunk, perplexed, lost];
+$disposition_you = array[happy, undercaffinated, lolcats, thisIsSeriousIamGaming
+$rpg_elements_strength = int();
+$rpg_elements_condition
+$rpg_elements_wisdom
+$rpg_elements_inteligence
+$rpg_elements_agility
+$rpg_elements_
+$rpg_elements_
+$rpg_elements_
+
+
+/* emotion variables */
+
+$total_love = int();
+$down_2_fuck = TRUE;
+$
+
+/* call some scripts... */
+
+
+
+?>
